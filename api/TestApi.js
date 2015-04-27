711f71d3cd5ad429d22169ffa0fc88e94493e405
Drew Miller <drew@wwdrew.com>
2015-04-27 16:22:57 +0100

    Playing with building the response data randomly

--- api/TestApi.js
+++ api/TestApi.js
@@ -1,6 +1,17 @@
+
+var Chance = require('chance');
+var chance = new Chance();
+
+function response () {
+    return {
+        name: chance.first(),
+        dob: chance.birthday()
+    };
+}
+
 module.exports = {
     'path': '/',
     'type': 'GET',
-    'response': 'This is the response for the / path'
+    'response': response()
 };
 
