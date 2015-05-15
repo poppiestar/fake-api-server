8ad3c091c0f574d8925c1d322b6944483ef1bc53
Drew Miller <drew@onpatrol.co>
2015-05-15 15:22:36 +0100

    Moving common functions into a module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var glob = require('glob');
 var fakeApi = require('./lib/api-server');
 var Constants = require('./lib/api-constants');
+var responseUtils = require('./lib/response-utils');
 
 var apiFiles = {};
 
@@ -11,7 +12,7 @@ var app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.use('/', fakeApi.api());
+app.use(fakeApi.api());
 
 glob('./api/**/*Api.js', function (err, files) {
     if (err) {
@@ -26,6 +27,11 @@ glob('./api/**/*Api.js', function (err, files) {
     }
 
     app.get('/config', function (req, res) {
+        // generate responses
+        for (var endpoint in apiFiles) {
+            apiFiles[endpoint].actual = responseUtils.generateResponse(apiFiles[endpoint].response);
+        }
+
         res.render('config', { constants: Constants, apiFiles: apiFiles });
     });
 
