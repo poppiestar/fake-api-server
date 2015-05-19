fa971c6727d9c6923c3f55a3457e104e5fcfd141
Drew Miller <drew@wwdrew.com>
2015-05-19 16:39:01 +0100

    Really rough moving main code out of main module

--- index.js
+++ index.js
@@ -1,54 +1,15 @@
-var express = require('express');
-var jf = require('jsonfile');
-var glob = require('glob');
-var fakeApi = require('./lib/api-server');
-var Constants = require('./lib/api-constants');
-var responseUtils = require('./lib/response-utils');
 
-var endpoints = {};
+var express = require('express');
+var fakeApi = require('./lib/api-server');
 
 var app = express();
-
-var configPage = '/config';
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(fakeApi.api());
 
-glob('./api/**/*Api.js', function (err, files) {
-    if (err) {
-        return console.log(err);
-    }
-
-    for (var file in files) {
-        var filename = files[file];
-        var endpoint = require(filename);
-
-        endpoints[endpoint.path] = endpoint;
-        endpoints[endpoint.path].actual = responseUtils.loadResponse(filename, endpoint);
-        fakeApi.addEndpoint(endpoints, endpoint);
-    }
-
-    app.get(configPage, function (req, res) {
-        var endpoint;
-
-        if (req.query.endpoint) {
-            endpoint = req.query.endpoint;
-        }
-        
-        res.render('config', { constants: Constants, endpoints: endpoints, endpoint: endpoints[endpoint], configPage: configPage });
-    });
-
-    app.post(configPage, function (req, res) {
-        res.status(200).send('Post received');
-    });
-
-    // handle 404s
-    app.use(function (req, res) {
-        res.status(404).render('404', { configPage: configPage });
-    });
-
+fakeApi.setup(__dirname, function () {
     app.listen(4040, function () {
         console.log('listening on port 4040');
     });
